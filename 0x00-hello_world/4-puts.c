3adaea05e9d8e3855d001e6ae69b2abd5fae993c
NyashaMasiraha <sebastiandannyjnr@gmail.com>
2023-06-16 06:36:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * *main - A program using puts function to print a line
  * Return:0 (Success)
  */
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts( "\"Programming is like building a multilingual puzzle");
 	return(0);
 }
