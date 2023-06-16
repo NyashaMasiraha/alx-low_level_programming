2ea67f426f7fbc83441e72a4a98f53cb579cff59
NyashaMasiraha <sebastiandannyjnr@gmail.com>
2023-06-16 08:04:06 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ int main(void)
 	printf("Size of long int: %lu byte(s)\n", (unsigned long) sizeof (c));
         printf("Size of long long int: %lu byte(s)\n", (unsigned long) sizeof (d));
 	printf("Size of float: %lu byte(s)\n", (unsigned long) sizeof (f));
-	return(0);
+	return (0);
 }
 
