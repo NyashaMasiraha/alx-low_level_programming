ceec7370ffb07e55536f68e1829a00e309b4228b
NyashaMasiraha <sebastiandannyjnr@gmail.com>
2023-06-19 16:41:46 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+ /**
+  * main - Entry point
+  * description: lowercase alphabet excluding q and e
+  * return: Always 0 (success)
+  */
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
