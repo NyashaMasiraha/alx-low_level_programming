e40a2aa10fbc76926c9d95f28de3e635212553b0
root <sebastiandannyjnr@gmail.com>
2023-07-04 12:11:52 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,18 +9,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int n;
+	int counter, compare_value;
 
-	n = 0;
-
-	while (s1[n] != '\0' && s2[n] != '\0')
+	counter = 0;
+	while (s1[counter] == s2[counter] && s1[counter] != '\0')
 	{
-		if (s1[n] != s2[n])
-		{
-			return (s1[n] - s2[n]);
-		}
-		n++;
+		counter++;
 	}
-
-	return (0);
+	compare_value = s1[counter] - s2[counter];
+	return (compare_value);
 }
