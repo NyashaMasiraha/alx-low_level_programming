aaee178c02df6ac532806d41f56dfbc5a9787853
root <sebastiandannyjnr@gmail.com>
2023-07-05 09:56:16 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * ste_string - a function that sets the value of a pointer to a char.
+ * @s: pointer to a pointer
+ * @to: character pointer
+ * Return: Always 0 (Success)
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
+
