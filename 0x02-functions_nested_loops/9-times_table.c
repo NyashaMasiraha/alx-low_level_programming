16504500b97d399ad61b98664aa7f7ce6f71c4ee
root <sebastiandannyjnr@gmail.com>
2023-07-05 04:15:42 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,23 +5,27 @@
 
 void times_table(void)
 {
-	int i, j, k,
+	int i, j, k, space;
 
-	for (i = 0; i < 10; i++;)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++;)
+		for (j = 0; j < 10; j++)
 		{
 			k = j * i;
-			(k < 10) ? 2 : 1;
+			(k < 10) ? (space = 2) : (space = 1);
 
-				if (k == 0)
+				if (j == 0)
+				{
 				_putchar('0');
+				}
 				else
-			{
+				{
 				_putchar(',');
 				_putchar(' ');
 				if (space == 2)
-				_putchar(' ');
+				{
+					_putchar(' ');
+				}
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
