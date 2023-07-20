36e9ea850e8bc226417a8f525fa4240e05ef4a81
root <sebastiandannyjnr@gmail.com>
2023-07-19 18:11:48 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog structure project
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Description: dog structure project
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+	dog_t;
+
+#endif/*DOG_H*/
+
