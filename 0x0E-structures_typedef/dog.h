8d69ae21e85de565fea72bceae3e12671400ec18
root <sebastiandannyjnr@gmail.com>
2023-07-21 06:28:30 -0700

    5-free_dogs.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,11 +20,14 @@ typedef struct dog
 /* function to initialize a dog struct */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
-/*function that prints a struct dog */
+/* function that prints a struct dog */
 void print_dog(struct dog *d);
 
 /* function that creates a new dog */
 dog_t *new_dog(char *name, float age, char *owner);
+
+/* a function that frees dogs */
+void free_dog(dog_t *d);
 
 #endif/*DOG_H*/
 
