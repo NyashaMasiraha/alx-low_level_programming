95b6fdd0d4224a53ce4b9d26310e845e64c53ccc
root <sebastiandannyjnr@gmail.com>
2023-08-10 05:18:09 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,29 +13,21 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd = open(filename, O_RDONLY);
-	char *buffer = malloc(sizeof(char) *letters);
-	ssize_t bytes_read = read(fd, buffer, letters);
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	ssize_t fd = open(filename, O_RDONLY);
+	char *buffer = malloc(sizeof(char) * letters);
+	ssize_t r = read(fd, buffer, letters);
+	ssize_t w = write(STDOUT_FILENO, buffer, r);
 
 	if (filename == NULL)
 	{
 		return (0);
 	}
+
 	if (fd == -1)
 	{
 		return (0);
 	}
-	if (bytes_read == -1)
-	{
-		close(fd);
-		return (0);
-	}
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		close(fd);
-		return (0);
-	}
+	free(buffer);
 	close(fd);
-	return (bytes_written);
+	return (w);
 }
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: the name of the file to create
+ * @text_content: is a NULL terminated string to write to the file
+ *
+ * Return: 1 (success), -1(failure or filename is NULL)
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, w;
+	int len = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	{
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+	{
+		return (-1);
+	{
+	close(o);
+	return (1);
+}
+
